56bbfa32ec79e19560d8a447caf42d9e75d445c8
Varun Pathak <varun_yashu88@yahoo.com>
2018-04-19 01:15:41 -0400

    Add files via upload

---
+++ Dictionary Key Value.py
@@ -0,0 +1,9 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+europe.keys()
+
+# Print out value that belongs to key 'norway'
+print(europe.keys())
+print(europe['norway'])
