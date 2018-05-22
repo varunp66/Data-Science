2f03d497663fba762a180627ebae6ee47ce0ea71
Varun Pathak <varun_yashu88@yahoo.com>
2018-05-21 23:42:17 -0400

    Delete Dictionary Key Value.py

--- Dictionary Key Value.py
+++
@@ -1,9 +0,0 @@
-# Definition of dictionary
-europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
-
-# Print out the keys in europe
-europe.keys()
-
-# Print out value that belongs to key 'norway'
-print(europe.keys())
-print(europe['norway'])
