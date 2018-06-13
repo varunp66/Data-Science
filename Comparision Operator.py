233b22c2b94d599a3c7c40b19da179a31403859a
Varun Pathak <varun_yashu88@yahoo.com>
2018-06-13 00:01:28 -0400

    Add files via upload

---
+++ Comparision Operator.py
@@ -0,0 +1,36 @@
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house < your_house)
+
+
+
+# Comparison of integers
+x = -3 * 6
+print(x >= -10)
+
+# Comparison of strings
+y = "test"
+print("test" <= y)
+
+# Comparison of booleans
+print(True > False)
+
+
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5 * 15 != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with a numeric
+print(True == 1)
