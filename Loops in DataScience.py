7b7e8dbedac84d5284a60a2d5462ad889bf68785
Varun Pathak <varun_yashu88@yahoo.com>
2018-08-05 23:28:18 -0400

    Add files via upload

---
+++ Loops in DataScience.py
@@ -0,0 +1,23 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn', 
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'australia':'vienna' }
+          
+# Iterate over europe
+for key, value in europe.items() :
+     print("the capital of " + str(key) + " is " + str(value))
+
+# in the above code the Key are the Countries and the Vlaue is Capitals    
+############################################################
+
+'''
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np_height :
+    print(str(x) + " inches")
+
+# For loop over np_baseball
+for x in np.nditer(np_baseball) :
+    print(x)
+'''
